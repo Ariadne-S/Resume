af85415b861aaf4c12f304b1d16c98e3fc731b8f
tonisefton <21288605+Ariadne-S@users.noreply.github.com>
2022-02-24 10:52:57 +1000

    Create project cards

--- resume-2022/components/experience.tsx
+++ resume-2022/components/experience.tsx
@@ -7,12 +7,21 @@ interface Project {
     icon: IconDefinition
     title: string,
     description: JSX.Element,
+    color: string,
     tags: string[]
+}
+
+const colors  = {
+    red: "hsl(0, 78%, 62%)",
+    cyan: "hsl(180, 62%, 55%)",
+    orange: "hsl(34, 97%, 64%)",
+    blue: "hsl(212, 86%, 64%)",
 }
 
 const projectData: Project[] = [
     {
         icon: findIconDefinition({prefix: 'fas', iconName: 'copy'}),
+        color: colors.cyan,
         title: "Custom Web Scrapers",
         description: <span>One of my first responsibilities at WORK180 was to build custom web scrapers to automate the
                             collection of job ads for new clients. This could involve a JSON/XML feed or a html parser
@@ -20,6 +29,87 @@ const projectData: Project[] = [
                             agility pack and depending on the complexity of the website configuring a headless crawl to
                             retrieve
                             generated html with Puppeteer.</span>,
+        tags: []
+    },
+    {
+        icon: findIconDefinition({prefix: 'fas', iconName: 'table-list'}),
+        color: colors.orange,
+        title: "Smart Scraper",
+        description: <span>
+            Collaborated to plan and implement the "Smart Scraper" project - a database driven scraper
+            infrastructure that reduced job automation set up from days to a couple of hours.
+        </span>,
+        tags: []
+    },
+    {
+        icon: findIconDefinition({prefix: 'fas', iconName: 'vials'}),
+        color: colors.blue,
+        title: "Smart Scraper Test Builder",
+        description: <span>
+            Encaptualisated logic to remove duplication and simplify the configuration of Smart
+                                Scraper
+                                tests
+        </span>,
+        tags: []
+    },
+    {
+        icon: findIconDefinition({prefix: 'fas', iconName: 'baby-carriage'}),
+        color: colors.blue,
+        title: "Structured Employer Benefits Data",
+        description: <span>
+            Designed and migrated a structred data schema for the Employer Benefits area to enable
+                            functionality such as comparing benifits and search. Worked alongside the customer success
+                            team to clean and migrate old data to the new structure. I
+
+                            Worked alongside UX to build their vision for the Employer Benefits tables
+        </span>,
+        tags: []
+    },
+    {
+        icon: findIconDefinition({prefix: 'fas', iconName: 'heart'}),
+        color: colors.red,
+        title: "Save job functionality",
+        description: <span>
+            Built the save job functionality, which entails an register/sign in gate, the ability to
+                            save
+                            a job
+                            and to remove a saved job from multiple places across the website. With google analytics
+                            events firing for each action.
+        </span>,
+        tags: []
+    },
+    {
+        icon: findIconDefinition({prefix: 'fas', iconName: 'heart'}),
+        color: colors.cyan,
+        title: "Port the frontend to NextJs typescript",
+        description: <span>
+            Spearheaded a greenfield project to port the jobsite frontend from a Razor/javascript React
+                            project to Typescript React NextJs, drastically improving page speed and improving developer
+                            experience
+        </span>,
+        tags: []
+    }
+    ,
+    {
+        icon: findIconDefinition({prefix: 'fas', iconName: 'heart'}),
+        color: colors.orange,
+        title: "Domain Migration",
+        description: <span>
+            Investigated and implemented NextJs internationalised routing and worked with the team to
+                            migrate the website from regionalised sub domains to sub paths to create a website with a
+                            more
+                            globalised feel.
+        </span>,
+        tags: []
+    }
+    ,
+    {
+        icon: findIconDefinition({prefix: 'fab', iconName: 'wordpress'}),
+        color: colors.blue,
+        title: "CMS Migration",
+        description: <span>
+            Migrate content and assets from Squidex to Wordpress
+        </span>,
         tags: []
     }
 ]
@@ -72,6 +162,8 @@ const ProjectsCallout: FC = () => {
 
 const ProjectGrid = styled.div`
     display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 `
 
 const Timeline: FC = () => {
@@ -88,16 +180,48 @@ const Timeline: FC = () => {
 
 const ProjectDisplay: FC<{ project: Project }> = ({project}) => {
     return (
-        <ProjectCard>
-            <FontAwesomeIcon icon={project.icon}/>
-            <h6>{project.title}</h6>
-            <p>{project.description}</p>
+        <ProjectCard color={project.color}>
+            <div>
+                <h6>{project.title}</h6>
+                <p>{project.description}</p>
+            </div>
+            <FontAwesomeIcon icon={project.icon} size={"2x"}/>
         </ProjectCard>
     )
 }
 
-const ProjectCard = styled.div`
-    max-width: 40%;
+const ProjectCard = styled.div<{color: string}>`
+    width: 280px;
+    font-size: 12px;
+    border-radius: 5px;
+    box-shadow: 0px 30px 40px -20px hsl(229, 6%, 66%);
+    padding: 30px;
+    padding-top: 40px;
+    margin: 20px;  
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-top: 5px solid ${props => props.color};
+    background-color: hsl(0, 0%, 98%);
+    
+    h6 {
+        line-height: 0.8;
+        margin-bottom: 50px;
+        margin-top: 100px;
+        margin: 0 auto;
+        line-height: 1.5;
+        color: hsl(234, 12%, 34%);
+        font-weight: 600;
+        font-size: 16px;
+    }
+    
+    p {
+        color: hsl(229, 6%, 66%);
+    }
+    svg {
+        align-self: flex-end;
+        color: ${props => props.color}
+    }
 `;
 
 const ExperienceContainer = styled.div`
